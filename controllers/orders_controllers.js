b9e997cc3eb605e8a2b1b9f34214602cbe15cfc2
OJ423 <osmith.dpm@gmail.com>
2024-09-06 18:05:29 +0100

    add order endpoints

---
+++ controllers/orders_controllers.js
@@ -0,0 +1,93 @@
+const jwt = require("jsonwebtoken");
+const { addOrder, removeOrderItem, removeOrder, fetchOrders, fetchOrderByUser, fetchOrder } = require("../models/orders_models");
+const { orderConfirmationEmail } = require("../utils/user_emails");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+exports.postOrder = async (req, res, next) => {
+  try {
+    const { user, body } = req;
+    const order = await addOrder(user.id, body);
+    const token = await jwt.sign(
+      { id: user.id, name: user.name }, JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+    orderConfirmationEmail(order)
+    res.status(201).send({order, token})
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+exports.deleteOrderItem = async (req, res, next) => {
+  try {
+    const { orderItemId } = req.params;
+    const { user } = req;
+    const orderItem = await removeOrderItem(user.id, orderItemId)
+    const token = await jwt.sign(
+      { id: user.id, name: user.name }, JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+    res.status(200).send({msg: "Order item deleted", orderItem:orderItem[0], newOrderTotal:orderItem[1],token})
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+exports.deleteOrder = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const { orderId } = req.params;
+    const order = await removeOrder(user.id, orderId)
+    const token = await jwt.sign(
+      { id: user.id, name: user.name }, JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+    res.status(200).send({msg: "Order deleted", order, token})
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+exports.getOrders = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const orders = await fetchOrders(user.id)
+    const token = await jwt.sign(
+      { id: user.id, name: user.name }, JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+    res.status(200).send({orders, token})
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+exports.getOrderByUser = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const orders = await fetchOrderByUser(user.id);
+    const token = await jwt.sign(
+      { id: user.id, name: user.name }, JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+    res.status(200).send({orders, token})
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+exports.getOrder = async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+    const order = await fetchOrder(orderId)
+    res.status(200).send(order)
+  }
+  catch(err) {
+    next(err)
+  }
+}
--- db/data/test-data/order_items.js
+++ db/data/test-data/order_items.js
@@ -1,22 +1,32 @@
 module.exports = [
   {
     order_id: 1,           
-    event_ticket_id: 1,    
+    event_ticket_id: 1,
+    ticket_price: 100.00,   
     quantity: 2,
   },
   {
     order_id: 2,           
-    event_ticket_id: 3,    
+    event_ticket_id: 3,
+    ticket_price: 100.00, 
     quantity: 1,
   },
   {
     order_id: 3,           
-    event_ticket_id: 4,    
+    event_ticket_id: 4, 
+    ticket_price: 75.00,     
     quantity: 3,
   },
   {
     order_id: 4,           
-    event_ticket_id: 5,    
+    event_ticket_id: 5, 
+    ticket_price: 150.00,     
     quantity: 2,
-  }
+  },
+  {
+    order_id: 1,           
+    event_ticket_id: 3,
+    ticket_price: 100.00,   
+    quantity: 1,
+  },
 ];
