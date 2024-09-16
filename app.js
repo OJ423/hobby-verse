9432742f74e49a04915278d9b3dae8c29b972c0e
OJ423 <osmith.dpm@gmail.com>
2024-09-16 23:04:44 +0100

    Fixed missing dependence in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const apiRouter = require('./routes/apiRouter')
 const {applicationErrors, customErrors, psqlErrors} = require("./errors/error_handling")
 const cors = require('cors');
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const app = express()
 app.use(cors(({
